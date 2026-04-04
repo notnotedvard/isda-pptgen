c28be821d2666d53131f6528682894e27af41d59
notnotnescap <97590612+nescapp@users.noreply.github.com>
2026-04-04 17:06:23 +0200

    updated template and formatted code

--- hymn_lyrics_generator.py
+++ hymn_lyrics_generator.py
@@ -1,32 +1,34 @@
 """This script generates a PowerPoint presentation with the lyrics of a hymn based on a template pptx file."""
 
 import os
-import time
 import shutil
 import sqlite3
+import time
+
 from pptx import Presentation
+
 from pptx_builder import *
 
-DB = sqlite3.connect('hymns.sqlite')
-CACHED_DB = sqlite3.connect('cache/hymns.sqlite')
+DB = sqlite3.connect("hymns.sqlite")
+CACHED_DB = sqlite3.connect("cache/hymns.sqlite")
 cursor = DB.cursor()
 cached_cursor = CACHED_DB.cursor()
 NUMBER_OF_HYMNS = cursor.execute("SELECT COUNT(*) FROM hymns").fetchone()[0]
 
 def generate_hymn(number:int):
-    """genrates slides for hymn number"""
+    """Genrates slides for hymn number"""
     # getting the data from the database
     hymn_name = cursor.execute("SELECT name FROM hymns WHERE id = ?", (number,)).fetchone()[0]
     print(f"Generating slides for hymn #{number:03}: {hymn_name}")
-    cursor.execute('SELECT verse_text, verse_number FROM verses WHERE hymn_id = ? ORDER BY verse_number', (number,))
+    cursor.execute("SELECT verse_text, verse_number FROM verses WHERE hymn_id = ? ORDER BY verse_number", (number,))
     verses = cursor.fetchall()
-    cursor.execute('SELECT refrain_text, refrain_position FROM refrains WHERE hymn_id = ? ORDER BY refrain_position', (number,))
+    cursor.execute("SELECT refrain_text, refrain_position FROM refrains WHERE hymn_id = ? ORDER BY refrain_position", (number,))
     refrains = cursor.fetchall()
 
     # creating a presentation
     template = Presentation("template.pptx")
     insert_song_title_slide(template, number, hymn_name)
-    
+
     if len(refrains) == 0: # if there are no refrains, add verses one by one
         for verse in verses[:-1]:
             insert_smart_chorus_slide(template, verse[1], verse[0])
@@ -89,8 +91,8 @@ for hymn_number in range(1, NUMBER_OF_HYMNS + 1):
 
 print("Caching db...")
 # make a copy in cache/hymns.sqlite
-if not os.path.exists('cache'):
-    os.makedirs('cache')
-shutil.copy('hymns.sqlite', 'cache/hymns.sqlite')
+if not os.path.exists("cache"):
+    os.makedirs("cache")
+shutil.copy("hymns.sqlite", "cache/hymns.sqlite")
 
 print(f"Done in {time.time() - start_time:.2f} seconds.")
