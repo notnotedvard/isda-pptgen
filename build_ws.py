1398f7f5d76c06f603cfbc2a6574778f02b2dfde
notnotnescap <97590612+nescapp@users.noreply.github.com>
2026-04-05 22:05:18 +0200

    python script to generate ppt for a ws

---
+++ build_ws.py
@@ -0,0 +1,85 @@
+"""Generates a sample presentation using the pptx_builder module."""
+
+import datetime
+from pptx import Presentation
+
+from ppt_builder import (
+    delete_template_slides,
+    insert_chorus_slide,
+    insert_end_slide,
+    insert_image_slide,
+    insert_simple_title_slide,
+    insert_scripture_slide,
+    insert_start_slide,
+    insert_title_with_logo_slide,
+    insert_video_slide,
+    merge_pptx,
+)
+
+def get_mission_spotlight_video():
+    """Downloads the mission spotlight and returns the path to the video file and the thumbnail image."""
+    return "example-files/gyp.mp4"
+
+# the following "insert" functions should be moved to ppt_builder.py and should be implemented there, but for now they are just placeholders here
+
+def insert_hymn(presentation, hymn_number):
+    """Inserts the slides for a hymn given its number."""
+
+def isert_images(path):
+    """Inserts the images for a given path."""
+
+def insert_thithes_and_offerings_slides(presentation, unallocated_offerings):
+    """Inserts the slides for the thithes and offerings given the unallocated offerings."""
+
+
+date = datetime.date.fromisoformat("2024-06-15")
+song_service_hymns = [526, 526]
+call_to_worship_scripture = "Psalm 147:1"
+opening_song_hymn = 473
+childrens_story_ppt = ""
+thermometers_slides = ""
+unallocated_offerings = "Combined Budget"
+special_item_video = "example-files/gyp.mp4"
+scripture_reading = "John 3:16"
+sermon_title = "God's Free Health Plan"
+preacher = "John Doe"
+meditation_video = ""
+closing_song_hymn = 633
+
+presentation = Presentation("template.pptx")
+insert_start_slide(presentation, date)
+insert_title_with_logo_slide(presentation, "Sabbath School Offering & Mission Spotlight")
+insert_video_slide(presentation, get_mission_spotlight_video(), "example-files/gyp-thumbnail.png", "Mission Spotlight")
+insert_title_with_logo_slide(presentation, "Song Service")
+for hymn_number in song_service_hymns:
+    insert_hymn(presentation, hymn_number)
+insert_title_with_logo_slide(presentation, "Welcome and Announcements")
+isert_images("announcements")
+insert_title_with_logo_slide(presentation, "Call to Worship")
+insert_scripture_slide(presentation, call_to_worship_scripture)
+insert_title_with_logo_slide(presentation, "Opening Song")
+insert_hymn(presentation, opening_song_hymn)
+insert_title_with_logo_slide(presentation, "Intercessory Prayer")
+insert_title_with_logo_slide(presentation, "Children's Story")
+# (optional) insert children's story ppt
+if childrens_story_ppt != "":
+    merge_pptx(presentation, childrens_story_ppt)
+insert_title_with_logo_slide(presentation, "Tithes and Offerings")
+# (optional) isert thermometers
+if thermometers_slides != "":
+    merge_pptx(presentation, thermometers_slides)
+insert_thithes_and_offerings_slides(presentation, unallocated_offerings)
+insert_title_with_logo_slide(presentation, "Special Music")
+if special_item_video != "":
+    insert_video_slide(presentation, special_item_video, "example-files/gyp-thumbnail.png", "Special Music")
+insert_title_with_logo_slide(presentation, "Scripture Reading")
+insert_scripture_slide(presentation, scripture_reading)
+insert_simple_title_slide(presentation, sermon_title) # should include preacher aswell
+insert_title_with_logo_slide(presentation, "Meditation")
+if meditation_video != "":
+    insert_video_slide(presentation, meditation_video, "example-files/gyp-thumbnail.png", "Meditation")
+insert_title_with_logo_slide(presentation, "Closing Song")
+insert_hymn(presentation, closing_song_hymn)
+insert_title_with_logo_slide(presentation, "Closing Prayer")
+insert_title_with_logo_slide(presentation, "Closing Remarks")
+insert_end_slide(presentation)
